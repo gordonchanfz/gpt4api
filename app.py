f11da5e26c71b1dc920da334f8e89e8dd1e022f2
gordonchanfz <77518307+gordonchanfz@users.noreply.github.com>
2024-03-25 01:47:49 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import g4f
+from g4f.api import Api
+
+list_ignored_providers = [
+    # "RetryProvider",
+    "GptForLove",
+    "ChatBase",
+    "Chatgpt4Online",
+    "ChatAnywhere",
+    "GptGo",
+    "You",
+]
+self_port = os.environ.get('PORT', '1337')
+
+if __name__ == "__main__":
+    print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
+    port=self_port
+    app = Api(engine=g4f,list_ignored_providers=list_ignored_providers, debug=True)
+    app.run(f"0.0.0.0:{port}", use_colors=True)

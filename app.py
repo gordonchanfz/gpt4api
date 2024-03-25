cceec883ff7e03fb4e129bb23c2eb6ff80f5b8bb
gordonchanfz <77518307+gordonchanfz@users.noreply.github.com>
2024-03-25 15:36:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
     port=self_port
     app = Api(engine=g4f,list_ignored_providers=list_ignored_providers, debug=True)
-    app.run(f"0.0.0.0:{port}", use_colors=True)
+    app.run(f"0.0.0.0:1337", use_colors=True)
